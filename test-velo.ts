8bfe945bc4302814da3b3c23caac5eecae8880d7
0xeye <97349378+0xeye@users.noreply.github.com>
2025-08-28 15:38:59 +0100

    chore: cleanup

--- test-velo.ts
+++
@@ -1,9 +0,0 @@
-import { VelodromeFetcher } from './dist/fetchers/velodrome.js';
-
-async function test() {
-  const fetcher = new VelodromeFetcher();
-  const prices = await fetcher.fetchPrices(10, [], new Map());
-  console.log('Velodrome returned', prices.size, 'prices for Optimism');
-}
-
-test().catch(console.error);
